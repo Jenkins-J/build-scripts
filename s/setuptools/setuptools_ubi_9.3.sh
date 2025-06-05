edfcd92a2b5fdff0277952c50d49ca0fe377925e
James Jenkins <James.Jenkins@ibm.com>
2025-06-05 16:35:10 -0400

    Remove tox testing from setuptools

--- s/setuptools/setuptools_ubi_9.3.sh
+++ s/setuptools/setuptools_ubi_9.3.sh
@@ -101,12 +101,6 @@ if ls */test_*.py > /dev/null 2>&1 && [ $test_status -ne 0 ]; then
     (python3 -m pytest) && test_status=0 || test_status=$?
 fi
 
-# Run tox if tox.ini is present and previous tests failed
-if [ -f "tox.ini" ] && [ $test_status -ne 0 ]; then
-    echo "Running tox..."
-    (python3 -m tox -e py39) && test_status=0 || test_status=$?
-fi
-
 # Final test result output
 if [ $test_status -eq 0 ]; then
     echo "------------------$PACKAGE_NAME:install_and_test_both_success-------------------------"
